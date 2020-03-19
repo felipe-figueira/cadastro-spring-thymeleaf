d89186ba2a131baf0a8a6a63ed4f0ae04d7c6b23
felipe-figueira <felipe.figueira13@gmail.com>
2020-03-19 12:36:04 -0300

    Carregando lista de pessoas cadastradas

--- src/main/java/com/projeto/spring/controller/PessoaController.java
+++ src/main/java/com/projeto/spring/controller/PessoaController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.projeto.spring.model.Pessoa;
 import com.projeto.spring.repository.PessoaRepository;
@@ -24,5 +25,11 @@ public class PessoaController {
 		return "cadastro/cadastropessoa";
 	}
 	
-	
+	@RequestMapping(method = RequestMethod.GET, value="/listapessoas")
+	public ModelAndView pessoas() {
+		ModelAndView andView = new ModelAndView("cadastro/cadastropessoa");
+		Iterable<Pessoa> pessoasIt = pessoaRepository.findAll();
+		andView.addObject("pessoas", pessoasIt);
+		return andView;
+	}
 }
