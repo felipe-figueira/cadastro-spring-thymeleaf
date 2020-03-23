4d7f5e8d91700a7a69ad8b3c19c33f709fafa63f
felipe-figueira <felipe.figueira13@gmail.com>
2020-03-22 22:19:03 -0300

    Adicionado filtro pelo nome das pessoas

--- src/main/java/com/projeto/spring/repository/PessoaRepository.java
+++ src/main/java/com/projeto/spring/repository/PessoaRepository.java
@@ -1,5 +1,8 @@
 package com.projeto.spring.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,5 +13,6 @@ import com.projeto.spring.model.Pessoa;
 @Transactional
 public interface PessoaRepository extends CrudRepository<Pessoa, Long>{
 
-	
+	@Query("select p from Pessoa p where p.nome like %?1%")
+	List<Pessoa> findPessoaByName(String nome);
 }
